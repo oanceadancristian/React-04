749aded7aa504769608cf7ab77557db33bbc99d9
Dan Cristian Oancea <oanceadancristian@gmail.com>
2022-11-07 12:57:31 +0200

    Homework 04 updated

--- homework-04/src/components/EpisodeItem/index.js
+++ homework-04/src/components/EpisodeItem/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import CharacterImage from '../CharacterImage';
 import './EpisodeItem.css';
+import Button from '@mui/material/Button';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 
 const EpisodeItem = (props) => {
   const { id, name, episode: episodeNr, characters } = props;
@@ -17,9 +20,44 @@ const EpisodeItem = (props) => {
         <div className="episode-id">Episode ID: {id}</div>
         <div className="episode-name">Episode name: {name}</div>
         <div className="episode-number">Episode number: {episodeNr}</div>
-        <button onClick={toggleCharacters} className="show-characters">
+        {/* <button onClick={toggleCharacters} className="show-characters">
           {active ? 'Hide characters' : 'Show characters'}
-        </button>
+        </button> */}
+        {active ? (
+          <Button
+            onClick={toggleCharacters}
+            sx={{
+              m: 2,
+              backgroundColor: '#efefef',
+              color: '#000000',
+              '&:hover': {
+                backgroundColor: '#f08d49',
+                color: '#ffffff',
+              },
+            }}
+            variant="contained"
+            startIcon={<ToggleOnIcon />}
+          >
+            Hide characters
+          </Button>
+        ) : (
+          <Button
+            onClick={toggleCharacters}
+            sx={{
+              m: 2,
+              backgroundColor: '#efefef',
+              color: '#000000',
+              '&:hover': {
+                backgroundColor: '#f08d49',
+                color: '#ffffff',
+              },
+            }}
+            variant="contained"
+            startIcon={<ToggleOffIcon />}
+          >
+            Show characters
+          </Button>
+        )}
       </div>
       <div className="character-container">
         {characters.map((character, index) => {
