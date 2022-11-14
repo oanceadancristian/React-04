3b67d34afbdac24a97416c7394305ee18e26dddc
Dan Cristian Oancea <oanceadancristian@gmail.com>
2022-11-14 04:04:25 +0200

    homework-06 update

--- homework-06/src/components/RickAndMortyApp/index.js
+++ homework-06/src/components/RickAndMortyApp/index.js
@@ -40,83 +40,90 @@ const RickAndMortyApp = () => {
         <h1>The Rick and Morty API</h1>
       </header>
       <main>
-        {randomCharacterList.map((randomCharacter) => {
-          const showCharacterStatus = () => {
-            let className = '';
-            if (randomCharacter.status === 'Alive') {
-              return (className = 'dynamic-green');
-            } else if (randomCharacter.status === 'Dead') {
-              return (className = 'dynamic-red');
-            } else {
-              return (className = 'dynamic-dark-gray');
-            }
-          };
+        <div className="random-character-container">
+          {randomCharacterList.map((randomCharacter) => {
+            const showCharacterStatus = () => {
+              let className = '';
+              if (randomCharacter.status === 'Alive') {
+                return (className = 'dynamic-green');
+              } else if (randomCharacter.status === 'Dead') {
+                return (className = 'dynamic-red');
+              } else {
+                return (className = 'dynamic-dark-gray');
+              }
+            };
 
-          const showCharacterGender = () => {
+            const showCharacterGender = () => {
+              return (
+                randomCharacter.gender.charAt(0).toUpperCase() +
+                randomCharacter.gender.slice(1)
+              );
+            };
+
+            const showCharacterSpecies = () => {
+              return (
+                randomCharacter.species.charAt(0).toUpperCase() +
+                randomCharacter.species.slice(1)
+              );
+            };
+
+            const showCharacterLocation = () => {
+              return (
+                randomCharacter.location.name.charAt(0).toUpperCase() +
+                randomCharacter.location.name.slice(1)
+              );
+            };
+
+            const showCharacterOrigin = () => {
+              return (
+                randomCharacter.origin.name.charAt(0).toUpperCase() +
+                randomCharacter.origin.name.slice(1)
+              );
+            };
+
             return (
-              randomCharacter.gender.charAt(0).toUpperCase() +
-              randomCharacter.gender.slice(1)
-            );
-          };
-
-          const showCharacterSpecies = () => {
-            return (
-              randomCharacter.species.charAt(0).toUpperCase() +
-              randomCharacter.species.slice(1)
-            );
-          };
-
-          const showCharacterLocation = () => {
-            return (
-              randomCharacter.location.name.charAt(0).toUpperCase() +
-              randomCharacter.location.name.slice(1)
-            );
-          };
-
-          const showCharacterOrigin = () => {
-            return (
-              randomCharacter.origin.name.charAt(0).toUpperCase() +
-              randomCharacter.origin.name.slice(1)
-            );
-          };
-
-          return (
-            <div className="random-character-details" key={randomCharacter.id}>
-              <img
-                src={randomCharacter.image}
-                alt={randomCharacter.name}
-                className="random-character-image"
-              />
-              <div className="random-character-info">
-                <div className="random-character-name">
-                  <Link
-                    to={`/episodes/${randomCharacter.id}/characters/${randomCharacter.id}`}
-                    className="random-character-link"
-                  >
-                    {randomCharacter.name}
-                  </Link>
-                </div>
-                <div className="random-character-species">
-                  <div className={showCharacterStatus()}></div>
-                  {showCharacterGender()} - {showCharacterSpecies()}
-                </div>
-                <div className="random-character-location">
-                  <span className="random-last-known-location">
-                    Last known location:
-                  </span>
-                  <div>{showCharacterLocation()}</div>
-                </div>
-                <div className="random-character-origin">
-                  <span className="random-first-seen-in">First seen in:</span>
-                  <div>{showCharacterOrigin()}</div>
+              <div
+                className="random-character-details"
+                key={randomCharacter.id}
+              >
+                <img
+                  src={randomCharacter.image}
+                  alt={randomCharacter.name}
+                  className="random-character-image"
+                />
+                <div className="random-character-info">
+                  <div className="random-character-name">
+                    <Link
+                      to={`/episodes/${randomCharacter.id}/characters/${randomCharacter.id}`}
+                      className="random-character-link"
+                    >
+                      {randomCharacter.name}
+                    </Link>
+                  </div>
+                  <div className="random-character-species">
+                    <div className={showCharacterStatus()}></div>
+                    {showCharacterGender()} - {showCharacterSpecies()}
+                  </div>
+                  <div className="random-character-location">
+                    <span className="random-last-known-location">
+                      Last known location:
+                    </span>
+                    <div>{showCharacterLocation()}</div>
+                  </div>
+                  <div className="random-character-origin">
+                    <span className="random-first-seen-in">First seen in:</span>
+                    <div>{showCharacterOrigin()}</div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-        <Link to="/episodes" className="episodes-link">
-          <button className="episodes-btn">See episodes</button>
-        </Link>
+            );
+          })}
+        </div>
+        <div className="episodes-link-btn-container">
+          <Link to="/episodes" className="episodes-link">
+            <button className="episodes-btn">See episodes</button>
+          </Link>
+        </div>
       </main>
     </>
   );
