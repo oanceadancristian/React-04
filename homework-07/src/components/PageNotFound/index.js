4addcded826d892d1a319381249333b3313cadc3
Dan Cristian Oancea <oanceadancristian@gmail.com>
2022-11-28 14:03:00 +0200

    updates

--- homework-07/src/components/PageNotFound/index.js
+++ homework-07/src/components/PageNotFound/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavbarNoAccount from '../NavbarNoAccount';
 import Navbar from '../Navbar';
+import Buttons from '../HomepageNoAccount/Buttons';
 import Typography from '@mui/material/Typography';
 
 const PageNotFound = () => {
@@ -8,22 +9,43 @@ const PageNotFound = () => {
 
   return (
     <>
-      {!user && <NavbarNoAccount />}
       {user && <Navbar />}
-      <Typography
-        variant="h1"
-        sx={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          color: 'black',
-          fontSize: '50px',
-          textAlign: 'center',
-        }}
-      >
-        Ooops... Page not found!
-      </Typography>
+      {user && (
+        <Typography
+          variant="h1"
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            color: 'black',
+            fontSize: '50px',
+            textAlign: 'center',
+          }}
+        >
+          Page not found!
+        </Typography>
+      )}
+      {!user && <NavbarNoAccount />}
+      {!user && (
+        <>
+          <Typography
+            variant="h1"
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              color: 'black',
+              fontSize: '50px',
+              textAlign: 'center',
+            }}
+          >
+            Access denied!
+            <Buttons />
+          </Typography>
+        </>
+      )}
     </>
   );
 };
