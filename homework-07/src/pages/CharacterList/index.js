3d137fe05abfa8eb815803902d7811904b1f10b5
Dan Cristian Oancea <oanceadancristian@gmail.com>
2022-12-02 23:29:24 +0200

    final project done

--- homework-07/src/pages/CharacterList/index.js
+++ homework-07/src/pages/CharacterList/index.js
@@ -72,13 +72,16 @@ const CharacterList = () => {
           if (error.response.status === 404) {
             setLoading(false);
             setCharacterPageError(error.response.data.error);
+            console.log(error.response);
           }
         } else if (error.request) {
           setLoading(false);
           setCharacterPageError('No response received');
+          console.log(error.request);
         } else {
           setLoading(false);
           setCharacterPageError(error.message);
+          console.log(error);
         }
       });
   }, [params.pageId, pageNumber, search, status, gender, species, dispatch]);
